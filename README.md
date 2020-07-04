db21c8d7ecaafdbd092cd93ab27fb849fbd36d55
shumtz <shumtztuber2@gmail.com>
2020-07-04 19:34:45 -0300

    feat: inicialize

---
+++ README.md
@@ -0,0 +1,7 @@
+# Adonis
+
+This is the fullstack boilerplate for AdonisJs, it comes pre-configured with.
+
+## Api bem simples
+
+Fiz essa api bem basica para estudar um pouco de AdonisJS.
---
+++ app/Controllers/Http/ListController.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const Task = use('App/Models/Task')
+const { validateAll } = use('Validator')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with lists
+ */
+class ListController {
+  async index ({ response }) {
+    const task = Task.all();
+    return task;
+  }
+  async create ({ request, response, auth }) {
+    try {
+
+      const validation = await validateAll(request.all(), {
+        task: 'required'
+      })
+
+      if (validation.fails()) {
+        return response.status(401).send({ message: validation.messages() })
+      }
+
+    const taskCreate = request.only(['task']);
+    const create = await Task.create(taskCreate)
+
+    return create;
+    } catch(err) {
+      return response.status(500).send({error: `${err}`});
+    }
+  }
+
+  async show ({ params, request, response, view }) {
+    const taskUpdate = await Task.query().where('id', params.id).firstOrFail()
+
+    return taskUpdate
+  }
+
+  async destroy ({ params, request, response }) {
+    const taskDelete = await Task.query().where('id', params.id).firstOrFail()
+
+    await taskDelete.delete()
+
+    return response.json({
+      "status": "sucess",
+      "message": "Task delete",
+      "data": taskDelete
+    })
+  }
+}
+
+module.exports = ListController
